244d81161d5c8f5c216e083aa0209ede4243e2fe
Anirudh Konidala <kidskodingclub@gmail.com>
2023-02-13 11:39:06 -0600

    created git repo for greenfoot game

---
+++ Poker.java
@@ -0,0 +1,11 @@
+import greenfoot.*;
+public class Poker extends World
+{
+    public Poker() {       
+        super(1280, 720, 1); 
+    }
+    private void makeGame() {
+        Color c = new Color(
+        
+    }
+}
